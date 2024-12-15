fe4a4f048cb94e7a9fda9e988af1448698e74211
JomanaMahmoud <jomanamahmoud53@gmail.com>
2024-12-15 21:12:32 +0200

    added when we run it opens the login page

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){res.render('index',{title:"express"})})
+app.get('/',function(req,res){res.render('login')})
   app.listen(3000);
+  
   
